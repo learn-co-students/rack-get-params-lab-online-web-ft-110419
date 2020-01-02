baeaffdbd781a6c0d92f7ca9b297dea418f96490
Ethan <ethan_gustafson@outlook.com>
2020-01-02 17:27:20 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -7,30 +7,30 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    if req.path.match(/items/)
-      @@items.each do |item|
+ 
+    if req.path.match(/items/) # if req.path matches /items/
+      @@items.each do |item| #iterate over each item, and it writes items
         resp.write "#{item}\n"
       end
 
     elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+      search_term = req.params["q"] # variable is equal to req.params method which takes in an argument.
+      resp.write handle_search(search_term) 
 
     elsif req.path.match(/cart/)
       if @@cart.empty?
       resp.write "Your cart is empty"
       else
-        @@cart.each do |item| 
+        @@cart.each do |item| #else we will iterate over what we have in the cart currently.
           resp.write "#{item}\n"
         end
       end
 
-    elsif req.path.match(/add/)
-      new_item = req.params["item"]
-      @@cart << new_item
-      if @@items.include?(new_item)
-      resp.write "added #{new_item}"
+    elsif req.path.match(/add/) #we can have multiple if statements as long as they have an end to separate them.
+      new_item = req.params["item"] # in the spec you will see "get '/add?item=Figs'"
+      if @@items.include?(new_item) # if the items array includes the item
+      @@cart << new_item # we add that new item to the cart.
+      resp.write "added #{new_item}" #we write "added (interpolation)"
       else
         resp.write "We don't have that item"
       end
@@ -48,3 +48,5 @@ class Application
     end
   end
 end
+
+# https://sailsjs.com/documentation/reference/request-req/req-params
